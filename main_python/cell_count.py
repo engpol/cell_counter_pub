b1773e40edcede037279963430eab040eca463da
engpol <137185112+engpol@users.noreply.github.com>
2025-01-14 19:39:08 +0000

    Update cell_count.py
    adding base64 image decoding for tiff file

--- main_python/cell_count.py
+++ main_python/cell_count.py
@@ -1,14 +1,28 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import base64
 from cellpose import models
 from cellpose import io
 from PIL import Image
 from matplotlib import pyplot as plt
 from matplotlib.colors import Normalize
 
+encoded_file_path = "cell_images/chosen_image.tiff"  # File containing Base64-encoded tiff image
+decoded_file_path = "cell_images/decoded_image.tiff" # File with decoded tiff image
+
+with open(encoded_file_path, "r") as encoded_file: # Read tiff image
+    encoded_content = encoded_file.read()
+    
+# Step 2: Decode the Base64 content
+decoded_content = base64.b64decode(encoded_content)
+
+# Step 3: Write the decoded binary data back to a TIFF file
+with open(decoded_file_path, "wb") as decoded_file:
+    decoded_file.write(decoded_content)
+
 # open image data and convert to Python from Java
-data = io.imread('cell_images/chosen_image.jpg')
+data = io.imread('cell_images/decoded_image.tiff')
 # run Cellpose on cytoplasm (grayscale)
 model = models.CellposeModel(gpu=False, model_type='cyto2')
 ch = [0, 0]
